553b82f7ba9a6d6c897cdccb850658e6852be347
Abhyudit <abhyuditjain@gmail.com>
2018-08-29 03:48:09 +0530

    challenge 8

--- ch8.ex
+++ ch8.ex
@@ -1,6 +1,6 @@
 defmodule Ch8 do
   def deck do
-    for r <- ranks, s <- suits, do: r <> " of " <> s
+    for r <- ranks(), s <- suits(), do: r <> " of " <> s
   end
 
   defp ranks do
@@ -9,5 +9,19 @@ defmodule Ch8 do
 
   defp suits do
     ~w(Spades Hearts Clubs Diamonds)
+  end
+
+  # find right angle triangle with integer side length less than n
+  def triangles(n) when n < 1, do: []
+
+  def triangles(n) do
+    for a <- 1..n,
+        b <- 1..n,
+        c <- 1..n,
+        c >= b,
+        b >= a,
+        a + b > c,
+        c * c == a * a + b * b,
+        do: {a, b, c}
   end
 end
