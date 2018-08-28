efe04b8455d66ceeddce85b77b4cf5df21b86498
Abhyudit Jain <abhyuditjain@gmail.com>
2018-08-28 15:30:20 +0530

    timer module and tests

--- fib/lib/fib.ex
+++ fib/lib/fib.ex
@@ -6,13 +6,12 @@ defmodule Fib do
     naive(number - 1) + naive(number - 2)
   end
 
-  def faster(number) do
-    faster(number, 0, 1)
+  def naive(a, b) do
+    naive(a + b)
   end
 
-  def compare(n \\ 40) do
-    IO.puts(~s{Naive: #{Fib.Timer.time(&naive/1, n)}})
-    IO.puts(~s{Tail Recursive: #{Fib.Timer.time(&faster/1, n)}})
+  def faster(number) do
+    faster(number, 0, 1)
   end
 
   defp faster(1, _acc1, acc2), do: acc2
